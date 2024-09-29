d38dafb89b57b18e47a65205efd40e6235297930
Areejslama <areegahmed449@gmail.com>
2024-09-29 22:41:57 +0300

    mange

---
+++ controllers/UsersController.js
@@ -0,0 +1,26 @@
+import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
+import Queue from 'bull';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const queue = new Queue('userQueue');
+    const { email, password } = req.body;
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
+
+    const users = await dbClient.db.collection('users');
+    users.findOne({ email }, async (err, result) => {
+      if (result) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+      const hashedPassword = sha1(password);
+      const { insertedId } = await users.insertOne({ email, password: hashedPassword });
+      const user = { id: insertedId, email };
+      queue.add({ userId: insertedId });
+      return res.status(201).json(user);
+    });
+  }
+
+export default UsersController;
